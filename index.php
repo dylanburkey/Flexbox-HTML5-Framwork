79ab0ca27a411db0c56dea0380adab6180f09629
Dylan Burkey <dylanburkey@outlook.com>
2013-03-23 16:40:21 -0400

    Updated Repo
    New Updated Structure

---
+++ index.php
@@ -0,0 +1,149 @@
+<!doctype html>
+<html lang="en" class="no-js">
+        <head>
+        <meta charset="utf-8">
+
+        <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+        <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+        <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+        <!--[if gt IE 8]><!--><!--<![endif]-->
+
+        <!-- DELETE LINE BELOW IF INCLUDED IN HTACCESS FILE -->
+        <title>Dylan Burkey - Front-End Web Developer in Cleveland, Ohio</title>
+        <!-- Startng Template -->
+        <meta name="description" content="Dylan Burkey is a Web Developer from Cleveland, Ohio. Dylan has an extesive background in front-end development working in PHP, ASP.NET, Ruby and Python enviorments.">
+        <meta name="author" content="Dylan Burkey - http://dylanburkey.com">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- link to any stylesheets below -->
+        <!-- link to normalize stylesheet - will reset default browser styles -->
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <!-- main app stylesheet -->
+        <link rel="alternate stylesheet" href="css/fontstyles.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <!-- external stylesheets -->
+       
+        <script src="http://ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.0.6-development-only.js"></script>
+        </head>
+        <body>
+<!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+<div id="w">
+<section class="redhead">
+
+</section>
+          <header id="masthead"><img role="banner" src="img/dylan-burkey-x2.png" width="266" height="66" alt="Dylan Burkey" longdesc="http://dylanburkey.com" /> 
+    <!-- <img src="img/dylanburkey-logo.png" width="5" height="525" alt="dylan burkey" longdesc="http://dylanburkey.com" title="DylanBurkey.com" role="banner" /> -->
+    <h1 id="banner">dylanburkey</h1>
+    <nav role="navigation">
+              <ul>
+        <li>home</li>
+        <li>about</li>
+        <li>store</li>
+        <li>local</li>
+        <li>contact</li>
+        <li>members only</li>
+      </ul>
+            </nav>
+    <!-- end nav -->
+    
+    <nav class="bg-lblue" id="subnav">
+              <ul>
+        <li>login</li>
+      </ul>
+            </nav>
+    <div class="clearfix"></div>
+    <!-- end #subnav --> 
+    
+  </header>
+          <form id="search" name="search" action="find-q.php" method="get" role="search" enctype="application/x-www-form-urlencoded">
+    <label for="search">search</label>
+    <input name="search" id="search" type="search" accept="text/plain" placeholder="search website" />
+  </form>
+          <!-- end #search --> 
+          <!-- end header -->
+          
+          <div id="main" role="main">
+    <main>
+              <article id="dylan" class="focus">
+        <h2>dylanburkey.com will be back soon with <span class="base-title"> &#8249;basis&#8250;</span></h2>
+        <p>Basis uses CSS Flexbox to structure the layouts included in the framework. The focus of <strong>BASIS</strong> is developing a semantic HTML5 and CSS/CSS3 Framework - utilizing Flexbox for all template structures.</p>
+        <p>BASIS is currently in alpha staging with portions of the framework still being brainstormed. If you are interested in BASIS and would like to learn more - visit the BASIS page on a GitHub.</p>
+        <p>The following features are standard in BASIS</p>
+        <ul>
+                  <li>a. Basis is developed in HTML(5), CSS(3), Javascript. </li>
+                  <li>b. Basis comes standard with rich HTML5 semantic templates.</li>
+                  <li>c. Basis uses PHP to create custom Flexbox templates. ** THIS IS ONLY IF YOU PLAN TO INSTALL BASIS AS A COMPLETE FRAMEWORK AND TEMPLATING APP. ** </li>
+                 <li> d. Basis is Mobile/Tablet/TV/ and Clock Radio Ready.</li>
+                  <li>e. Basis supports IE 6 and above, Opera, Firefox, Chrome, Safari, Chromium, Canary and Aruara. </li>
+                </ul>
+        </p>
+      </article>
+            </main>
+    <!-- end main --> 
+  </div>
+          <!-- end #main -->
+          
+          <footer class="box" role="contentinfo">
+    <article class="flex1">
+             <nav class="footer sitelinks">
+             	<ul>
+                	<li><a href="/" title="home" rel="first">home</a></li>
+                   		<li><a href="?p=about" title="about" rel="next">about</a></li>
+     
+        <li><a href="?p=work" title="store" rel="next">my work</a></li>
+        <li><a href="?p=dev" title="My Development" rel="archives">development tips</a></li>
+        <li><a href="?p=contact" title="Contact Me" rel="alternate">about</a></li>
+        <li><a href="?p=follow" title="Follow My Day to Day Life" rel="bookmark" data-prefetch="true">my every movement (sign-up)</a></li>
+
+                </ul>
+             
+             </nav>
+            </article>
+    <!-- end .flex1 -->
+    
+    <article class="flex2 bg-lblue"> </article>
+    <!-- end .col fixed -->
+    
+    <article class="flex1">
+
+		<figure id="contact-me">
+        	<img src="https://twimg0-a.akamaihd.net/profile_images/2689449949/8684e0e94666bb0b86a83ee3886824e8.jpeg" alt="Dylan Burkey" title="Dylan Burkey" role="img" />
+        </figure>
+            </article>
+    <!-- end .col fluid --> 
+    
+    <!--
+                  ##### For Cookie Laws ## NOT REQUIRED BY DEFAULT ### 
+                <script type="text/javascript" src="http://assets.futurecdn.net/js/fp_cookie.min.js" async defer></script>
+                --> 
+  </footer>
+          <!-- end footer --> 
+        </div>
+<!-- end #w --> 
+
+<!--[if lt IE 7 ]>
+    <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+    <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.0/jquery.validate.min.js"></script> 
+	 <script type="text/javascript" src="http://assets.futurecdn.net/js/fp_cookie.min.js" async defer></script>
+
+  <![endif]--> 
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script> 
+<script src="http://static.zencodez.net/js/jquery.css3finalize-v3.x.min.js"></script> 
+<script src="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.10.1/jquery-ui.min.js"></script> 
+<script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+</body>
+        </html>
+
+
